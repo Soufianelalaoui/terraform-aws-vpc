5c8ee10ea085f8cffddec46bc547f6869451ae04
lalaoui-soufiane <soufiane.lalaoui@prestarocket.com>
2021-09-22 12:25:18 +0200

    create instances

--- main.tf
+++ main.tf
@@ -92,3 +92,13 @@ resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCgb4KJX+Rtdm4rfAllGeviFxt1ONlj8zwbHaaoCIbpBr52re3xT1LND/tiQyool0qL9iZQIjd89//EPXNzlvNPXM+XJhN5A2zgTmHanAoJt+6N6LDJRCUYfRI9ooJzkWsraB7IqAPe1/lxb8OH0LZjS+OYoGn/0zVzlEeKZlSJSSf+GF98AHKcWxvUVpU/E++Q7fmsHdCCYDzxf6SGpUzgVC+WiIJN/u+c2uAIF0ZJ/mdgBZhOi85ISuVfnXeYKvxVfZry7jsLjVCJrLOBBdWCY5twHgsCdjKWDqkfVRVNoam/2e+QKsJnyxg8ajlYLVrQCiIXgf9S6KjMc4VtvOqP"
 }
+
+resource "aws_instance" "instance" {
+  for_each = toset(var.vpc_azs)
+
+  ami = data.aws_ami.nat_ami.id
+  instance_type = "t2.micro"
+  source_dest_check = false
+  vpc_security_group_ids = [aws_security_group.allow_nat.id]
+  subnet_id = aws_subnet.public[each.value].id
+ }
